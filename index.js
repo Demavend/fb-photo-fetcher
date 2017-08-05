fa68e693a4b9cc8f2e031171cd3182cf45b892e2
Demavend <demavend12345@gmail.com>
2017-08-05 12:49:43 +0300

    create navbar

---
+++ index.js
@@ -0,0 +1,19 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '161335491091695',
+        xfbml: true,
+        version: 'v2.10'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/ru_RU/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
