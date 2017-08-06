9300e806e96127e3e53c33549447b38a05b191e2
Demavend <demavend12345@gmail.com>
2017-08-06 10:43:21 +0300

    create login function

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 window.fbAsyncInit = function() {
     FB.init({
         appId: '161335491091695',
+        autoLogAppEvents: true,
         xfbml: true,
         version: 'v2.10'
     });
-    FB.AppEvents.logPageView();
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            var uid = response.authResponse.userID;
+            accessToken = response.authResponse.accessToken;
+            $(".loggedin").css('display', 'block');
+            $(".loggedoff").css('display', 'none');
+            console.log(uid);
+            console.log(accessToken);
+            document.querySelector('.loggedin').addEventListener('click', function goLogIn() {
+                FB.logout(function(response) {
+                    // handle the response
+                    $(".loggedin").css('display', 'none');
+                    $(".loggedoff").css('display', 'block');
+                }); //FB.logoff
+            }, false);
+        } else if (response.status === 'not_authorized') {
+            //User is logged into Facebook, but not your App
+            $(".loggedin").css('display', 'none');
+            $(".loggedoff").css('display', 'block');
+        } else {
+            // User is not logged into Facebook at all
+            $(".loggedin").css('display', 'none');
+            $(".loggedoff").css('display', 'block');
+            document.querySelector('.loggedoff').addEventListener('click', function goLogIn() {
+                FB.login(function(response) {
+                    // handle the response
+                    $(".loggedin").css('display', 'block');
+                    $(".loggedoff").css('display', 'none');
+                }, {
+                    scope: 'email,user_likes'
+                }); //FB.login
+            }, false);
+        }
+    });
 };
 
 (function(d, s, id) {
