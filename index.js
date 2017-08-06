b97dced194e2e4fa022877a69a9fa982d1198eee
dlym <lymda@unicsoft.com.ua>
2017-08-06 20:04:05 +0300

    Refactor index

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-var oauth_url = 'https://www.facebook.com/dialog/oauth/';
-oauth_url += '?client_id=161335491091695';
-oauth_url += '&redirect_uri=' + 'http://fb-photo-app.zzz.com.ua/';
-oauth_url += '&scope=user_about_me,email,user_location,user_photos,publish_actions,user_birthday,user_likes';
-window.fbAsyncInit = function() {
-  console.log('before init')
+const FB_SCOPE = 'user_about_me,email,user_location,user_photos,publish_actions,user_birthday,user_likes';
+const APP_ID = '161335491091695';
+const VERSION = 'v2.10';
+var oauth_url = `https://www.facebook.com/dialog/oauth/` +
+    `?client_id=${APP_ID}` +
+    `&redirect_uri="http://fb-photo-app.zzz.com.ua/"` +
+    `&scope=${FB_SCOPE}`;
+
+window.fbAsyncInit = () => {
     FB.init({
-        appId: '161335491091695',
+        appId: APP_ID,
         autoLogAppEvents: true,
         xfbml: true,
-        version: 'v2.10'
+        version: VERSION
     });
     FB.getLoginStatus(function(response) {
-      console.log('resp', response)
-
+        // TODO: use switch case loop
+        // TODO: move css change classes to method and call it with params
         if (response.status === 'connected') {
             var uid = response.authResponse.userID;
             accessToken = response.authResponse.accessToken;
@@ -20,43 +23,42 @@ window.fbAsyncInit = function() {
             $(".loggedoff").css('display', 'none');
             console.log(uid);
             console.log(accessToken);
-            document.querySelector('.loggedin').addEventListener('click', function goLogIn() {
-                FB.logout(function(response) {
-                    // handle the response
-                    $(".loggedin").css('display', 'none');
-                    $(".loggedoff").css('display', 'block');
-                }); //FB.logoff
-            }, false);
         } else if (response.status === 'not_authorized') {
             //User is logged into Facebook, but not your App
-            $(".loggedin").css('display', 'none');
-            $(".loggedoff").css('display', 'block');
+            // $(".loggedin").css('display', 'none');
+            // $(".loggedoff").css('display', 'block');
             window.top.location = oauth_url;
         } else {
             // User is not logged into Facebook at all
-            $(".loggedin").css('display', 'none');
-            $(".loggedoff").css('display', 'block');
-            document.querySelector('.loggedoff').addEventListener('click', function goLogIn() {
-                FB.login(function(response) {
-                    // handle the response
-                    $(".loggedin").css('display', 'block');
-                    $(".loggedoff").css('display', 'none');
-                    window.top.location = oauth_url;
-                }, {
-                    scope: 'user_about_me,email,user_location,user_photos,publish_actions,user_birthday,user_likes'
-                }); //FB.login
-            }, false);
+            // $(".loggedin").css('display', 'none');
+            // $(".loggedoff").css('display', 'block');
         }
+
+        document.querySelector('.loggedin').addEventListener('click', function() {
+            FB.logout(changeClass); //FB.logoff
+        }, false);
+
+        document.querySelector('.loggedoff').addEventListener('click', function() {
+            FB.login(function(response) {
+                // handle the response
+                // $(".loggedin").css('display', 'block');
+                // $(".loggedoff").css('display', 'none');
+
+                window.top.location = oauth_url;
+            }, {
+                scope: FB_SCOPE
+            }); //FB.login
+        }, false);
     });
 };
 
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+// (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) {
+//         return;
+//     }
+//     js = d.createElement(s);
+//     // js.id = id;
+//     // js.src = 'https://connect.facebook.net/en_US/sdk.js';
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
